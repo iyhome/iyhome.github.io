2028405312b8fbe877740547a39e3fb6ba137cf5
iyhome <iyhome@users.noreply.github.com>
2025-04-05 13:46:09 +0000

    deploy: iyhome/SereinBlog@a616fc5117ae3ab38e38cdac2f934729e22a266b

--- js/custom.js
+++ js/custom.js
@@ -52,7 +52,7 @@ jQuery(document).ready(function($) {
 function runtime() {
 	window.setTimeout("runtime()", 1000);
 	/* 请修把这里的建站时间换为你自己的 */
-    let startTime = new Date('05/23/2023 08:00:00');
+    let startTime = new Date('12/01/2019 08:00:00');
     let endTime = new Date();
     let usedTime = endTime - startTime;
     let days = Math.floor(usedTime / (24 * 3600 * 1000));

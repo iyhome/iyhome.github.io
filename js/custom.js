30107621c5c3375804849ff3f26518293e8d1e99
iyhome <iyhome@users.noreply.github.com>
2025-04-05 11:13:55 +0000

    deploy: iyhome/SereinBlog@6ccedbaeaadad5fddcd9a04254424bc0f481e9c4

--- js/custom.js
+++ js/custom.js
@@ -70,3 +70,25 @@ function runtime() {
         + ((seconds < 10) ? '0' : '') + seconds + ' 秒 ';
 }
 runtime();
+
+/* ---   Config2.离开进入网站的标题变化   ---*/
+/* 离开当前页面时修改网页标题，回到当前页面时恢复原来标题 */
+window.onload = function() {
+	var OriginTitile = document.title;
+	var titleTime;
+	document.addEventListener('visibilitychange', function() {
+	  if(document.hidden) {
+		$('[rel="icon"]').attr('href', "/failure.ico");
+		$('[rel="shortcut icon"]').attr('href', "/failure.ico");
+		document.title = '哦~该回码头了';
+		clearTimeout(titleTime);
+	  } else {
+		$('[rel="icon"]').attr('href', "/favicon.ico");
+		$('[rel="shortcut icon"]').attr('href', "/favicon.ico");
+		document.title = '哈~又整到薯条了';
+		titleTime = setTimeout(function() {
+		  document.title = OriginTitile;
+		}, 2000);
+	  }
+	});
+  }
